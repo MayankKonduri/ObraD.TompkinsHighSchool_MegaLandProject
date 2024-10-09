bb6168694aa3c8e5d226f2c0129ddff13d6834d0
Ayan Gupta <160621180+AyanGupta06@users.noreply.github.com>
2024-10-09 11:03:00 -0500

    Commit after Testing

---
+++ Project/src/EndGamePanel.java
@@ -0,0 +1,49 @@
+package Project.src;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+
+public class EndGamePanel extends JPanel {
+
+    private BufferedImage loading;
+    private JFrame jFrame;
+
+
+    public EndGamePanel(JFrame frame) {
+
+        setSize(1920, 1040);
+        setLayout(null);
+        this.jFrame = frame;
+
+
+        try {
+            loading = ImageIO.read((new File("Project\\src\\Images\\Loading_v7.jpg")));
+
+        } catch (Exception ah) {
+            ah.printStackTrace();
+            System.out.println("Error Loading Images: " + ah.getMessage());
+        }
+
+        setVisible(true);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+//        Graphics bg = buffer.getGraphics();
+        g.drawImage(loading, 0, 0, 1920, 1050, null);
+//        g.drawImage(buffer, 0, 0, null);
+    }
+
+
+
+
+}
