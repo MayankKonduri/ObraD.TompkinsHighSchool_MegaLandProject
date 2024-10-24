6fa1a612b2b79bfca98fd7cae0c8b1c0ca6ce0cb
nischalkonduri <nischal.konduri@gmail.com>
2024-10-24 10:01:35 -0500

    modifiedingamerules

--- Project/src/InGameRulesPanel.java
+++ Project/src/InGameRulesPanel.java
@@ -1,8 +1,10 @@
 package Project.src;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
@@ -10,11 +12,13 @@ public class InGameRulesPanel extends JPanel{
     private BufferedImage rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10, rule11, rule12, ruleEnd;
     private JButton nextPage = new JButton(">");
     private JButton previousPage = new JButton("<");
-    private JButton homeButton = new JButton("Game");
+    private JButton homeButton = new JButton("Home");
     public int pageCount1 = 0;
     public int pageCount = 0;
     private GamePanel gamePanel;
     private boolean isRulesVisible;
+    public JLabel ruleInstructions = new JLabel("Press the \">\" to flip.");
+
 
 
 
@@ -44,11 +48,21 @@ public class InGameRulesPanel extends JPanel{
             ah.printStackTrace();
             System.out.println("Error Loading Images: " + ah.getMessage());
         }
-        nextPage.setBounds(1845, 480, 50, 80);
-        nextPage.setOpaque(true);
+        nextPage.setBounds(1850, 480, 70, 80);
+        nextPage.setFont(new Font("Georgia", Font.BOLD, 40));
+        nextPage.setOpaque(false);
+        nextPage.setBackground(Color.black);
+        nextPage.setFocusPainted(false);
+        nextPage.setBorderPainted(false);
+        nextPage.setContentAreaFilled(false);
 
-        previousPage.setBounds(10, 480, 50, 80);
-        previousPage.setOpaque(true);
+        previousPage.setBounds(10, 480, 70, 80);
+        previousPage.setOpaque(false);
+        previousPage.setBackground(Color.black);
+        previousPage.setFocusPainted(false);
+        previousPage.setBorderPainted(false);
+        previousPage.setContentAreaFilled(false);
+        previousPage.setFont(new Font("Georgia", Font.BOLD, 40));
 
         homeButton.setBounds(10, 10, 100, 30);
 
@@ -66,7 +80,39 @@ public class InGameRulesPanel extends JPanel{
         homeButton.addActionListener(e -> {
             toggleGame();
         });
-        homeButton.setFont(new Font("Georgia", Font.PLAIN, 20));
+
+        homeButton.setBounds(10, 10, 100, 40);
+        homeButton.setFont(new Font("Georgia", Font.BOLD, 20));
+        homeButton.addActionListener(e -> {
+            LoadingPanel loadingPanel = new LoadingPanel(frame);
+            loadingPanel.setPreferredSize(new Dimension(1920,1040));
+
+            JScrollPane scrollPane1 = new JScrollPane(loadingPanel);
+            scrollPane1.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+            scrollPane1.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+            add(scrollPane1);
+            frame.setContentPane(scrollPane1);
+
+            frame.pack();
+            frame.revalidate();
+            frame.repaint();
+            frame.setVisible(true);
+
+        });
+
+        homeButton.setFocusPainted(false);
+        homeButton.setBackground(Color.black);
+        homeButton.setForeground(Color.white);
+        homeButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                homeButton.setBorder(new LineBorder(Color.white, 1));
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                homeButton.setBorder(null);
+            }
+        });
 
         add(nextPage);
         add(previousPage);
@@ -89,13 +135,24 @@ public class InGameRulesPanel extends JPanel{
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         if(pageCount == 0) {
-            g.drawImage(rule1, 0, 0, 1920, 1010, null);
+            g.setColor(Color.BLACK);
+            g.fillRect(0, 0, 960, 1010);
+            ruleInstructions.setBounds(150, 465, 960, 80);
+            ruleInstructions.setFont(new Font("Georgia", Font.BOLD, 60));
+            ruleInstructions.setForeground(Color.WHITE);
+            add(ruleInstructions);
+            g.drawImage(rule1, 960, 0, 960, 1010, null);
             previousPage.setVisible(false);
+            homeButton.setBounds(970, 10, 100, 40);
+
         } else if(pageCount == 1) {
+            ruleInstructions.setVisible(false);
             g.drawImage(rule2, 0, 0, 960, 1010, null);
             g.drawImage(rule3, 960, 0, 960, 1010, null);
             previousPage.setVisible(true);
             nextPage.setVisible(true);
+            homeButton.setBounds(10, 10, 100, 40);
+
         }else if(pageCount == 2){
             g.drawImage(rule4, 0, 0, 960, 1010, null);
             g.drawImage(rule5, 960, 0, 960, 1010, null);
@@ -116,11 +173,17 @@ public class InGameRulesPanel extends JPanel{
             g.drawImage(rule11, 960, 0, 960, 1010, null);
             previousPage.setVisible(true);
             nextPage.setVisible(true);
+            ruleInstructions.setVisible(false);
         }else if(pageCount == 6){
+            ruleInstructions.setVisible(true);
             g.drawImage(rule12, 0, 0, 960, 1010, null);
-            g.drawImage(ruleEnd, 960, 0, 960, 1010, null);
+            g.setColor(Color.BLACK);
+            g.fillRect(960, 0, 960, 1010);
             previousPage.setVisible(true);
             nextPage.setVisible(false);
+            ruleInstructions.setBounds(970+ 150, 465, 960, 80);
+            ruleInstructions.setFont(new Font("Georgia", Font.BOLD, 60));
+            ruleInstructions.setText("End of the Rulebook.");
         }
     }
 }
